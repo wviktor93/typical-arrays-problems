8d8704198d53b0abba86dba9548a4df04d5659b2
Victoria Triputina <triputina.victoria@gmail.com>
2020-02-19 23:39:54 +0300

    feat: solved typical array

--- src/index.js
+++ src/index.js
@@ -1,12 +1,45 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+  let res = 0;
+  // console.log(array);
+  if (array === undefined || array.length == 0) {
+    res = 0;
+  } else {
+    res = array[0];
+    for (let i = 1; i < array.length; i++) {
+      if (array[i] < res) {
+        res = array[i];
+      }
+    }
+  }
+  return res;
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+  let res = 0;
+  if (array === undefined || array.length == 0) {
+    res = 0;
+  } else {
+    res = array[0];
+    for (let i = 1; i < array.length; i++) {
+      if (array[i] > res) {
+        res = array[i];
+      }
+    }
+  }
+  return res;
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+  let res = 0;
+  if (array === undefined || array.length == 0) {
+    return res;
+  } else {
+    res = array[0];
+    for (let i = 1; i < array.length; i++) {
+      res += array[i];
+    }
+    return res / array.length;
+  }
+  
 }
